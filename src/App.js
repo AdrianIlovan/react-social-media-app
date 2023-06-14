d0542e85ae8935af0a5524b78f3b5606c4dcb310
AdrianIlovan <adrian88ilovan@yahoo.com>
2023-06-14 13:56:46 +0100

    Module 3 React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  const names = ["Adrian", "Marble-Vigilante", "Wonder-Woman", "Spiderman", "SpiderKiller"];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className='App'>
+    {names.map((name, index) => {
+      return <h1> {name} </h1>
+})}
+  </div>
   );
 }
+  // const age = 19;
+  // const isBlue = true;
+
+  // return(  
+  //  <div className="App">{age >= 18 ? <h1>Yes</h1> : <h1>No</h1>}
+  //  <h1 style={{color: isBlue ? "blue" : "green"}}>Compton</h1>
+  //  {isBlue && <button>This is a button</button>}
+  //  </div>
+  // );
+
+  // const Job = (props) => {
+  //   return (
+  // <div>  
+  //    <h1>{props.salary}</h1>
+  //    <h1>{props.position}</h1>
+  //    <h1>{props.company}</h1>
+  // </div>
+  //     );
+  // }
+  
+
 
 export default App;
