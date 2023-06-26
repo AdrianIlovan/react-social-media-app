2c6b32bc4af785646c944e65bf2dddf51feec321
Adrian Ilovan <adrian88ilovan@yahoo.com>
2023-06-26 14:41:27 +0100

    To do List

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import './App.css';
-//import {User} from './User';
-import { useState } from "react";
+import React, { useState } from "react";
 
 
 function App() {
-  return <div className='App'>Hello World</div>;
+  const [todoList, setTodoList] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  const handleChange = (event) => {
+    setNewTask(event.target.value);
+  }
+
+  const addTask = () => {
+    setTodoList([...todoList, newTask]);
+  };
+
+  return (
+    <div className='App'>
+      <div className='addTask'>
+        <input onChange={handleChange}/>
+        <button onClick={addTask}> Add Task</button>
+      </div>
+      <div className='list'>
+        {todoList.map((task) => {
+          return <h1>{task}</h1>
+        })}
+      </div>
+    </div>
+    )
 }
 
 
