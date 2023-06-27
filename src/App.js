de4382d2f594930bd6c327f23bc6124314cd8f5f
Adrian Ilovan <adrian88ilovan@yahoo.com>
2023-06-27 17:14:22 +0100

    Simple CSS Added

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function App() {
         {todoList.map((task) => {
             return <div>
                       <h1>{task.taskName}</h1>
-                      <button className='button'
+                      <button className="button"
                       onClick ={() => deleteTask(task.id)}>X</button>
                    </div>
         })}
